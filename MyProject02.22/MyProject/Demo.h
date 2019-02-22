3657cc27af8d2b9a57d062c3013ba213f90e9735
wanghaoyu001 <46880572+wanghaoyu001@users.noreply.github.com>
2019-02-22 13:52:06 +0800

    Add files via upload
    在之前的基础上实现了对各种可能结果的计数

---
+++ MyProject02.22/MyProject/Demo.h
@@ -0,0 +1,50 @@
+class decisionnoed
+{
+public:
+	struct dividest
+	{
+		int rows;
+		int column;
+		int value;
+		int resultTrue;
+		int resultFalse;
+		int valueTrue;
+		int valueFalse;
+	};
+	dividest divs[100];
+	dividest results[100];
+
+	int col;
+	//char results;
+	char tb;
+	char fb;
+	int column;
+	char value[100];
+	char *const a = value;
+	
+	void dividest();//拆分的列号以及参考值的输入
+	void split_function();//根据所给参考值进行拆分
+	void output();//输出满足条件的拆分列表
+	void outputmy_data();//输出原列表
+	void uniquecounts();//对各种可能结果进行计数
+
+	char *const my_data[15][5] =
+	{
+	{ "slashdot","USA","yes","18","None" },
+	{ "google","France","yes","23","Premium" },
+	{ "digg","USA","yes","24","Basic" },
+	{ "kiwitobes","France","yes","23","Basic" },
+	{ "google","UK","no","21","Premium" },
+	{ "(direct)","New Zealand","no","12","None" },
+	{ "(direct)","UK","no","21","Basic" },
+	{ "google","USA","no","24","Premium" },
+	{ "slashdot","France","yes","19","None" },
+	{ "digg","USA","no","18","None" },
+	{ "google","UK","no","18","None" },
+	{ "kiwitobes","UK","no","19","None" },
+	{ "digg","New Zealand","yes","12","Basic" },
+	{ "google","UK","yes","18","Basic" },
+	{ "kiwitobes","France","yes","19","Basic" }
+	};
+};
+
